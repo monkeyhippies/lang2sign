7ac7b16ea8d9ab3fe7fea311cf4b9e6750ab3548
Michael Liu <michael_liu2@yahoo.com>
2019-09-20 11:40:25 -0700

    Update last commit

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+data:
+	./utils/download_en_asl_gloss_data.sh
 word-embeddings:
 	./utils/download_glove_embeddings.sh
--- utils/download_glove_embeddings.sh
+++ utils/download_glove_embeddings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EMBEDDINGS_DIR=lang2sign/lang2gloss/embeddings/
+EMBEDDINGS_DIR=pretrained-embeddings/
 mkdir -p $EMBEDDINGS_DIR
 wget -P $EMBEDDINGS_DIR http://nlp.stanford.edu/data/wordvecs/glove.6B.zip
 unzip $EMBEDDINGS_DIR/glove.6B.zip -d $EMBEDDINGS_DIR
