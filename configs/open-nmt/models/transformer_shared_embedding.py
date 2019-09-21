98ef4eb2699535a584334803a5c33df56c79fb10
Michael Liu <michael_liu2@yahoo.com>
2019-09-20 21:29:13 -0700

    Update last commit

---
+++ configs/open-nmt/models/transformer_shared_embedding.py
@@ -0,0 +1,16 @@
+# taken and copied from https://github.com/OpenNMT/OpenNMT-tf/blob/master/config/models/transformer_shared_embedding.py
+"""A Transformer model sharing all embeddings and softmax weights."""
+
+import tensorflow as tf
+import opennmt as onmt
+
+def model():
+  return onmt.models.Transformer(
+      num_layers=2,
+      num_units=256,
+      num_heads=4,
+      ffn_inner_dim=1024,
+      dropout=0.1,
+      attention_dropout=0.1,
+      relu_dropout=0.1,
+      share_embeddings=onmt.models.EmbeddingsSharingLevel.ALL)
