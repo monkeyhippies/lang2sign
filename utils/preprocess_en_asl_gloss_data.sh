bff58d3b8d4fed44229f511db93a64d8443824a8
Michael Liu <michael_liu2@yahoo.com>
2019-09-20 17:07:29 -0700

    Update last commit

--- utils/preprocess_en_asl_gloss_data.sh
+++ utils/preprocess_en_asl_gloss_data.sh
@@ -3,5 +3,5 @@
 RAW_DIR=data/raw/lang2gloss/enasl/
 PREPROCESSED_DIR=data/preprocessed/lang2gloss/enasl/
 
-mkdir -p PREPROCESSED_DIR
+mkdir -p $PREPROCESSED_DIR
 cp -r $RAW_DIR/. $PREPROCESSED_DIR/
