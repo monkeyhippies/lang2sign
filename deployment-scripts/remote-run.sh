c3d46542477c49e2727dfd0fdadb95faa726d3f1
Michael Liu <michael_liu2@yahoo.com>
2019-09-30 14:11:12 -0700

    Specify partition in deploy.sh script

--- deployment-scripts/remote-run.sh
+++ deployment-scripts/remote-run.sh
@@ -6,4 +6,4 @@ mkdir secrets;
 echo -n "__AWS_ACCESS_KEY_ID__" > secrets/aws_access_key_id;
 echo -n "__AWS_SECRET_ACCESS_KEY__" > secrets/aws_secret_access_key;
 sudo make deps install;
-sudo nohup make PARTITION_ID=__PARTITION_ID__ NUM_PARTITIONS=__NUM_PARTITIONS__ create-video-lookup &
+sudo nohup make PARTITION_ID=__PARTITION_ID__ NUM_PARTITIONS=__NUM_PARTITIONS__ create-video-lookup >> output.log 2>&1 &
