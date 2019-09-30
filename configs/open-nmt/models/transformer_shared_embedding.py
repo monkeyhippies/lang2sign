cac1fa06fab0cc2708dc819359b19de899f29265
Michael Liu <michael_liu2@yahoo.com>
2019-09-29 18:50:47 -0700

    Fix styling errors and add linting

--- configs/open-nmt/models/transformer_shared_embedding.py
+++ configs/open-nmt/models/transformer_shared_embedding.py
@@ -1,17 +1,15 @@
 # taken and copied from https://github.com/OpenNMT/OpenNMT-tf/blob/master/config/models/transformer_shared_embedding.py
 """A Transformer model sharing all embeddings and softmax weights."""
-
-import tensorflow as tf
 import opennmt as onmt
 
 def model():
-  return onmt.models.Transformer(
-	source_inputter=onmt.inputters.WordEmbedder(
-		vocabulary_file_key="source_words_vocabulary",
-		embedding_size=300),
-	target_inputter=onmt.inputters.WordEmbedder(
-          vocabulary_file_key="target_words_vocabulary",
-          embedding_size=300),
+    return onmt.models.Transformer(
+        source_inputter=onmt.inputters.WordEmbedder(
+            vocabulary_file_key="source_words_vocabulary",
+            embedding_size=300),
+        target_inputter=onmt.inputters.WordEmbedder(
+            vocabulary_file_key="target_words_vocabulary",
+            embedding_size=300),
     num_layers=6,
     num_units=300,
     num_heads=6,
